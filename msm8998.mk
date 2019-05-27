b4ef83bbbb818e247677e192562728cf13f58c7a
barracuda7 <barracudaking@gmail.com>
2019-05-28 08:48:26 +1000

    add openweathermap

--- msm8998.mk
+++ msm8998.mk
@@ -158,6 +158,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiDoze
 
+# Build openweather
+    OpenWeatherMapProvider
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
