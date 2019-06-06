edeb6dc511fb639ced68fa10cb2d4c24f6f4a99f
barracuda7 <barracudaking@gmail.com>
2019-06-06 14:15:27 +1000

    sync with ArianK16a

--- msm8998.mk
+++ msm8998.mk
@@ -158,9 +158,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiDoze
 
-# Build openweather
-PRODUCT_PACKAGES += \
-    OpenWeatherMapProvider
+# Device mapper verity
+PRODUCT_SYSTEM_VERITY_PARTITION=/dev/block/bootdevice/by-name/system
+PRODUCT_VENDOR_VERITY_PARTITION=/dev/block/bootdevice/by-name/cust
+$(call inherit-product, build/target/product/verity.mk)
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -280,10 +281,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.xiaomi_msm8998-libperfmgr
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    android.hardware.power@1.1-service-qti
 
 # Properties
 include $(LOCAL_PATH)/vendor_prop.mk
@@ -303,7 +301,6 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
-    init.target.performance.rc \
     init.target.rc \
     ueventd.qcom.rc
 
@@ -327,6 +324,8 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.0 \
     libril-wrapper \
     librmnetctl
 
