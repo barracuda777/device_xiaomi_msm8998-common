835544d2f28273e948276fe16f25e1327f81825b
barracuda7 <barracudaking@gmail.com>
2019-06-13 19:02:24 +1000

    Fix Stuff

--- msm8998.mk
+++ msm8998.mk
@@ -158,10 +158,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiDoze
 
-# Build openweather
-PRODUCT_PACKAGES += \
-    OpenWeatherMapProvider
-
 # Device mapper verity
 PRODUCT_SYSTEM_VERITY_PARTITION=/dev/block/bootdevice/by-name/system
 PRODUCT_VENDOR_VERITY_PARTITION=/dev/block/bootdevice/by-name/cust
