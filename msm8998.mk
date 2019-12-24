64f10f2c98cf3383a95d9780317d933b9a26a480
barracuda7 <barracudaking@gmail.com>
2019-12-24 11:09:07 +1100

    make SCHEDUTIL CPU Governor default

--- msm8998.mk
+++ msm8998.mk
@@ -314,7 +314,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.target.performance.rc
 
 PRODUCT_PACKAGES += \
     init.panel_info.sh \
