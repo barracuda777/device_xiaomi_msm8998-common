ce374a0bbe992a92e0463fe5c0c56a8da7b70d48
barracuda7 <barracudaking@gmail.com>
2020-04-19 02:40:34 +1000

    revert

--- msm8998.mk
+++ msm8998.mk
@@ -148,10 +148,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Snap
 
-# Dialer
-PRODUCT_PACKAGES += \
-    Dialer
-
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
